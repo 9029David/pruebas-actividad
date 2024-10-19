798f9717aa0e686c83e1b499903c0e2e0a942659
GFGaldeano <gustavo_galdeano@yahoo.com.ar>
2024-10-19 11:46:36 -0300

    primer commit de la caja de pandora hecho por gustavo

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+const laCajaDePandora = (numero) => {
+    if (numero % 2 === 0) {
+      // Si el número es par, se convierte a binario
+      return numero.toString(2);
+    } else {
+      // Si el número es impar, se convierte a hexadecimal
+      return numero.toString(16); 
     }
+  };
+
+  console.log(laCajaDePandora(123)); // imprime "7b"
+  console.log(laCajaDePandora(456)); // imprime "1c2"
+
+  
